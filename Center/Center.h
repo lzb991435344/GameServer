b10fbf7a7ff52e5e634fc136979abeff1d11ccb4
lzb991435344 <991435344@qq.com>
2019-04-13 22:12:01 +0800

    Center.h
    集群代码补充

--- Center/Center.h
+++ Center/Center.h
@@ -67,11 +67,56 @@ public:
 	 int QueryService(const std::string& name, GetServiceClientCallback* callback,
 	                  ClientCallback* client_cb = NULL, Router* router = NULL,
 					  void* route_param = NULL);
-private:	
+	
+	/**
+     * 根据合同缓存获得客户端对象
+     * @param cache 合同缓存对象
+     * @param client_cb 预期每个新的 Client 所注册的默认回调，用来接收连接、中断、收听通知。
+     * @param router 路由器对象
+     * @param route_param 路由参数
+     * @return 客户端对象指针，无需主动 delete，因为会缓存起来
+     */
+	 Client* GetClientByContracts(ContractCache* cache, ClientCallback* client_cb,
+								Router* router, void* route_param);
+	 
+    /**
+     * 获得存放集群的 ZK 基础路径
+     * @return ZNode 基础路径
+     */
+	 inline const std::string& cluster_prefix() const{
+		 return CLUSTER_PREFIX;
+	 }
 	 
 	 
+    /**
+     * 获得建立进程用的 ZK 标记 (Create flags)
+     * @return zk 的 create flags
+     */
+	 inline int process_flags() const{
+		 return process_flags;
+	 }
+	 // 在 ZK 写入本进程对此服务的合约
+	 void AddProcessContract(const std::string& service_name, 
+							const std::string& contract_data);
+	 //清理相关的对象
+	 void CloseClient(Client* client);
+	 
+	 //设置合同缓存
+	 void SetContractsCache(const std::string& service_name,ContractCache& cache);
+	 
+	 // 建立存储节点父目录时，增加一个监听器，监听这些节点增加和删除变化
+	 virtual void CreatePrefixNode();
+	 
+	 // 初始化 zookeeper 客户端连接，会修改 ZKMAP_KEY_PREFIX 为集群专用路径
+	 virtual int Init(Config* config = NULL);
 	 
 	 
+	/**
+     * 清理掉生成的客户端对象
+     * @param client 客户端对象
+     * @param content 相关的服务名字
+     */
+	 void ClearClientMember(Client* client, const std::string& content);
 	 
-	 
+private:	 
  };
